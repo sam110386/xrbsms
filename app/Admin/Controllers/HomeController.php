4bcb40d20e397a3d08524f83ff65287190964351
semanticglobal <sgstest2505@gmail.com>
2018-11-01 12:25:57 +0530

    Dashboard modification

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -15,20 +15,54 @@ class HomeController extends Controller
         return $content
             ->header('Dashboard')
             ->description('Description...')
-            ->row(Dashboard::title())
             ->row(function (Row $row) {
+                $row->column(12, function (Column $column) {
+                    $column->append("<style>
+                            .title {
+                                font-size: 50px;
+                                color: #636b6f;
+                                font-family: 'Raleway', sans-serif;
+                                font-weight: 100;
+                                display: block;
+                                text-align: center;
+                                margin: 20px 0 10px 0px;
+                            }
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
+                            .links {
+                                text-align: center;
+                                margin-bottom: 20px;
+                            }
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
-
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
+                            .links > a {
+                                color: #636b6f;
+                                padding: 0 25px;
+                                font-size: 12px;
+                                font-weight: 600;
+                                letter-spacing: .1rem;
+                                text-decoration: none;
+                                text-transform: uppercase;
+                            }
+                        </style>
+                        <div class='title'>
+                            Welcome Admin
+                        </div>");
                 });
             });
+            
+            // ->row(Dashboard::title());
+            // ->row(function (Row $row) {
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::environment());
+            //     });
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::extensions());
+            //     });
+
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::dependencies());
+            //     });
+            // });
     }
 }
